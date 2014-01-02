59d0f1c8077a2202f6ba553838f89fbe1aac87d1
baozhizhi <baozhizhi850709@gmail.com>
2014-01-02 19:10:01 +0800

    1.修正第一版本,可以浏览选择手机本身的文件

--- src/com/lightreader/bzz/Activity/StartPageActivity.java
+++ src/com/lightreader/bzz/Activity/StartPageActivity.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 import android.view.Window;
 
 
-public class StartPage extends Activity {
+public class StartPageActivity extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 		//this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//全屏
@@ -23,9 +23,9 @@ public class StartPage extends Activity {
     class loadhandler implements Runnable{
         public void run() {
             //startActivity(new Intent(getApplication(),MainActivity.class));
-        	Intent intent = new Intent(StartPage.this,MainActivity.class);  
-            StartPage.this.startActivity(intent); 
-        	StartPage.this.finish();
+        	Intent intent = new Intent(StartPageActivity.this,MainActivity.class);  
+            StartPageActivity.this.startActivity(intent); 
+        	StartPageActivity.this.finish();
         }
         
     }
